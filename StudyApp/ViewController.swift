1895abaf3148b62a603306113fac45024a67efa1
Miyu Sato <62976492+miyu0001@users.noreply.github.com>
2021-05-12 23:59:14 +0900

    Initial Commit

---
+++ StudyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyApp
+//
+//  Created by 佐藤未悠 on 2021/05/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
