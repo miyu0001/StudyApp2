12d6b0dbd6d25ece01b9151108c736b3ed2d482a
Miyu Sato <62976492+miyu0001@users.noreply.github.com>
2021-05-18 01:47:13 +0900

    投稿画面の作成

--- StudyApp/ViewController/MainViewController.swift
+++ StudyApp/ViewController/MainViewController.swift
@@ -8,8 +8,32 @@
 import Foundation
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController ,UITableViewDataSource,UITableViewDelegate{
+    
+    @IBOutlet var timelineTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        timelineTableView.dataSource = self
+        timelineTableView.delegate = self
+        
+        let nib = UINib(nibName: "TimelineTableViewCell", bundle: Bundle.main)
+        timelineTableView.register(nib, forCellReuseIdentifier: "Cell")
+        
+        //TavleViewの不要な線を消す
+        timelineTableView.tableFooterView = UIView()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! TimelineTableViewCell
+        //内容
+        
+        
+        return cell
     }
 }
---
+++ StudyApp/ViewController/TimelineTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  TimelineTableViewCell.swift
+//  StudyApp
+//
+//  Created by 佐藤未悠 on 2021/05/17.
+//
+
+import UIKit
+
+class TimelineTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var userNameLavel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var commentTextView: UITextView!
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        userImageView.layer.cornerRadius = userImageView.bounds.width / 2
+        userImageView.clipsToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
