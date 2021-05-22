39019a8550c8005fe3632176df94573515b747de
Miyu Sato <62976492+miyu0001@users.noreply.github.com>
2021-05-22 15:51:01 +0900

    NavigationControllerの設定

--- StudyApp/Main/BigViewController.swift
+++ StudyApp/Main/BigViewController.swift
@@ -21,24 +21,6 @@ class BigViewController: TabmanViewController {
         bar.layout.transitionStyle = .snap
         addBar(bar, dataSource: self, at: .top)
         
-        bar.layout.transitionStyle = .snap
-                addBar(bar.systemBar(), dataSource: self, at: .top)
-                bar.backgroundColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
-                bar.layout.contentInset = UIEdgeInsets(top: 0.0, left: 70.0, bottom: 0.0, right: 70.0)
-                bar.buttons.customize { (button) in
-                    button.tintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-                    button.selectedTintColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
-                }
-                bar.indicator.overscrollBehavior = .compress
-                bar.indicator.weight = .light
-                self.navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
-                // ナビゲーションバーのアイテムの色　（戻る　＜　とか　読み込みゲージとか）
-                self.navigationController?.navigationBar.tintColor = .white
-                // ナビゲーションバーのテキストを変更する
-                self.navigationController?.navigationBar.titleTextAttributes = [
-                // 文字の色
-                    .foregroundColor: UIColor.white
-                ]
     }
     //子コントローラーの選択
     private lazy var viewControllers: [UIViewController] = {
